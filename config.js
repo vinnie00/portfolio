5b11e1d8a90c08f57f7e110aa3b5063db48f1ddd
Collin <collinvaccaro00@gmail.com>
2020-08-17 18:12:40 -0600

    removed path prefix

--- config.js
+++ config.js
@@ -7,7 +7,6 @@ module.exports = {
   manifestThemeColor: '#663399',
   manifestDisplay: 'standalone',
   manifestIcon: 'src/assets/img/website-icon.png',
-  pathPrefix: `/portfolio/`, // This path is subpath of your hosting https://domain/portfolio
   heading: 'Collin Vaccaro',
   subHeading: 'Full stack developer. Full time student',
 
